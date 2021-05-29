153fbec76781df50936ed5870dc437d286ca3700
edsont8 <edsont8@hotmail.com>
2021-05-29 11:23:52 -0300

    Title

--- src/main/java/com/project/bootcamp/BootcampApplication.java
+++ src/main/java/com/project/bootcamp/BootcampApplication.java
@@ -3,6 +3,7 @@ package com.project.bootcamp;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,9 +16,9 @@ public class BootcampApplication {
 	}
 
 	@Bean
-	public OpenAPI customOpenAPI() {
+	public OpenAPI customOpenAPI(@Value("${application.description}") String description) {
 		return new OpenAPI().info(new Info()
-				.title("Titulo")
+				.title(description)
 				.version("1.0")
 				.termsOfService("http://swagger.io/terms")
 				.license(new License()
